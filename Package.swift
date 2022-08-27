61cf801b44ad22cbce46235231f91075e482b58e
Kamaal <kamaal.f1@gmail.com>
2022-08-27 18:29:23 +0200

    Supporting Swift 5.5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,7 +16,7 @@ let package = Package(
         .package(url: "https://github.com/kamaal111/XiphiasNet.git", "7.0.0" ..< "8.0.0"),
         .package(url: "https://github.com/Quick/Quick.git", "4.0.0" ..< "5.0.0"),
         .package(url: "https://github.com/Quick/Nimble.git", "9.0.0" ..< "10.0.0"),
-        .package(url: "https://github.com/kamaal111/MockURLProtocol", "0.1.0" ..< "0.2.0"),
+        .package(url: "https://github.com/kamaal111/MockURLProtocol", "0.1.1" ..< "0.2.0"),
     ],
     targets: [
         .target(
